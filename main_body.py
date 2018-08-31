2d06ea855347fb725e1ff6bc970098ac61b78818
MadeYoga <vngla21@gmail.com>
2018-08-31 21:31:39 +0700

    test

--- main_body.py
+++ main_body.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -42,4 +44,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
