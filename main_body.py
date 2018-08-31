91f601ecfdea8f3345552085dc409431c81ca95b
MadeYoga <vngla21@gmail.com>
2018-08-31 21:42:32 +0700

    first commit

--- main_body.py
+++ main_body.py
@@ -40,7 +40,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event.message.text)
+        )
 
 
 if __name__ == "__main__":
